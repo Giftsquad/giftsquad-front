171e311216e7341674ae5a23a3dea8f04f59b999
tomlrd <thomas.laroudie@gmail.com>
2025-09-22 11:30:23 +0200

    Revert "style : login and signup"

---
+++ app/auth/_layout.js
@@ -0,0 +1,38 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import { theme } from '../../styles/theme';
+
+export default function AuthLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.components.tabBar.activeTintColor,
+        tabBarInactiveTintColor: theme.components.tabBar.inactiveTintColor,
+        tabBarStyle: {
+          backgroundColor: theme.components.tabBar.backgroundColor,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name='signup'
+        options={{
+          title: "S'inscrire",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='person-add' size={24} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name='login'
+        options={{
+          title: 'Se connecter',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='person' size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
