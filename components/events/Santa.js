ae36dda0d0b76cde2d731e107b75541d1089228f
hyzeuline <iseline.voison@gmail.com>
2025-09-24 16:56:59 +0200

    style: modal santa

--- components/events/Santa.js
+++ components/events/Santa.js
@@ -140,7 +140,6 @@ export default function Santa({ event, setEvent }) {
       const assignedBy = event.event_participants.find(
         participant => participant.user._id === owner.assignedBy
       );
-      console.log('ici =>', assignedBy);
       return assignedBy.user.firstname;
     } else if (type === 'assignedTo') {
       // fonction pour révéler le prénom de la personne qu'un autre participant a tiré au sort
@@ -324,7 +323,11 @@ export default function Santa({ event, setEvent }) {
                       setModal2Visible(true);
                     }}
                   >
-                    <Ionicons name='eye-outline' size={24} color='green' />
+                    <Ionicons
+                      name='eye-outline'
+                      size={24}
+                      color={theme.colors.primary}
+                    />
                   </TouchableOpacity>
                 )}
               </View>
@@ -336,7 +339,7 @@ export default function Santa({ event, setEvent }) {
         {/* Modal 2 pour révéler le tirage au sort de chacun  */}
         <Modal visible={modal2Visible} transparent={true}>
           <View style={styles.modalOverlay}>
-            <View style={styles.modal}>
+            <View style={styles.modal2}>
               <View
                 style={{
                   width: '100%',
@@ -563,7 +566,7 @@ export default function Santa({ event, setEvent }) {
         </View>
       </Modal>
 
-      {/* Bouton de suppression d'événement - Seulement pour les administrateurs */}
+      {/* Bouton de suppression d'événement - Seulement pour les administrateurs ou de quitter l'événement pour les participants */}
       {isOrganizer && (
         <TouchableOpacity style={styles.deleteButton} onPress={deleteEvent}>
           <FontAwesome5
@@ -818,6 +821,24 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.5)',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  modal2: {
+    margin: 'auto',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: '95%',
+    height: 200,
+    padding: 22,
+    alignItems: 'flex-start',
+    gap: 20,
+    shadowColor: '#020202ff',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
   modal: {
     margin: 'auto',
