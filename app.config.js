b977acd0834dc055c321253acbc0598bb0ce781b
tomlrd <thomas.laroudie@gmail.com>
2025-09-25 16:43:37 +0200

    fix eas

--- app.config.js
+++ app.config.js
@@ -29,6 +29,9 @@ export default {
     plugins: ['expo-router'],
     extra: {
       API_URL: process.env.API_URL,
+      eas: {
+        projectId: '2f0473dd-d49f-4fe8-8644-8ae4516c7d09',
+      },
     },
   },
   cli: {
